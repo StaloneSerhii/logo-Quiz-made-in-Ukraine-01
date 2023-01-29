e579aec1041d0b3c2871ef52e4faa98f28546eb8
Serhii <sergio.fly4405@gmail.com>
2023-01-29 17:05:43 +0200

    sdasd

--- src/partials/level1/levelRend.js
+++ src/partials/level1/levelRend.js
@@ -1,21 +1,21 @@
 export const level1 = [{
     idName: "0",
     answer: "molokia",
-    logoHidden: 'molokia.f7ee194a.png',
-    logo: 'molokia-open.eb1b02fe.jpg',
+    logoHidden: 'molokia.png',
+    logo: 'molokia-open.jpg',
     info: "samsun its company"
 },
 {
     idName: "1",
     answer: "samsung",
-    logoHidden: 'samsung.08d95e2b.png',
-    logo: 'samsung-open.99a89ec2.png',
+    logoHidden: 'samsung.png',
+    logo: 'samsung-open.png',
     info: "samsun its company"
 },
 {
     idName: "1",
     answer: "samsung",
-    logoHidden: 'samsung.08d95e2b.png',
-    logo: 'samsung-open.99a89ec2.png',
+    logoHidden: 'samsung.png',
+    logo: 'samsung-open.png',
     info: "samsun its company"
 }]
