b1997954aa97323754a73fb3993ff59c4e667d53
Serhii <sergio.fly4405@gmail.com>
2023-01-30 20:47:52 +0200

    fac

--- src/partials/level1/levelRend.js
+++ src/partials/level1/levelRend.js
@@ -2,36 +2,36 @@
 const level1 = [{
     idName: "0",
     answer: "мівіна",
-    logoHidden: 'http://programki.com.ua/uploads/posts/2023-01/1675102824_mivina.png',
-    logo: 'http://programki.com.ua/uploads/posts/2023-01/1675102805_mivina-2.png',
+    logoHidden: 'https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328545463_1214855842571179_5469837124243712290_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=hBpKdCE-NH0AX_c4KbQ&_nc_ht=scontent.fifo4-1.fna&oh=00_AfCdMwQnT4dhTK6NfHCOGdkx2N_YUkcI9dKBuYfuQ99OGw&oe=63DCFB20',
+    logo: 'https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328232347_3149410092017716_7729609557883975706_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=LgH3OTxH46EAX_n6jTe&tn=cokD2Vnt1Oy3Wifo&_nc_ht=scontent.fifo4-1.fna&oh=00_AfCC9uQfN4gt0HFDvs4kIXg5MhRtTOcQTyW-45ag0ldxhQ&oe=63DD3741',
     info: "Мівіна – товарний знак, розроблений у Харкові корпорацією «Техноком», що займається виробництвом харчових напівфабрикатів.«Мівіна» є загальною назвою вермішелі швидкого приготування в українських споживачів"
 },
 {
     idName: "1",
     answer: "пакко",
-    logoHidden: 'http://programki.com.ua/uploads/posts/2023-01/1675102784_pakko-open.jpg',
-    logo: 'http://programki.com.ua/uploads/posts/2023-01/1675102762_6649028-pakko.jpg',
+    logoHidden: 'https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328159968_884399546197559_2369597404956451073_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=JtnKR2VGA38AX9mnbI0&_nc_ht=scontent.fifo4-1.fna&oh=00_AfA-ssXqU9FkInUBncvPUNsEpi3sMizHFTWCAcNXy4y9LA&oe=63DD4EBC',
+    logo: 'https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328245007_910042510358148_3980371885354044060_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=ysvE9y-4nYQAX_gTSI7&_nc_ht=scontent.fifo4-1.fna&oh=00_AfAbP4ClLMZlfChQbI64ujzfmrBLQqzGdzF-Im9MrwW-tw&oe=63DC807D',
     info: "«Вопак» — західноукраїнська торгова мережа. Загальна кількість магазинів у мережі 67 супермаркетів. Вони розташовані у Львівській, Тернопільській, Івано-Франківській, Чернівецькій, Закарпатській, Волинській, Житомирській та Хмельницькій областях."
 },
 {
     idName: "2",
     answer: "опілля",
-    logoHidden: 'http://programki.com.ua/uploads/posts/2023-01/1675102817_opillia-open.jpg',
-    logo: 'http://programki.com.ua/uploads/posts/2023-01/1675102746_opillia.png',
+    logoHidden: 'https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/327010834_691259966028981_6489095128817911222_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=cExS00BuPv0AX9CGzeJ&_nc_ht=scontent.fifo4-1.fna&oh=00_AfBGd3hi9vRFA66NiHDDPO9mlwokiEsPIIR5YBkrF2SFsQ&oe=63DC9055',
+    logo: 'https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328146365_5754896334588584_2146856469801665400_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=My3QvAMQlbIAX8Ot4uZ&_nc_ht=scontent.fifo4-1.fna&oh=00_AfC0ssO58AJEuihQx_WrzDCbaS0lG3ZMvt-1KhCtlJqFng&oe=63DC5527',
     info: "Тернопільська пивоварня «Опілля» — підприємство, зайняте у галузі виробництва та реалізації пива та квасу. Розташоване у місті Тернополі."
 }, 
 {
     idName:"3",
     answer:"садочок",
-    logoHidden:"http://programki.com.ua/uploads/posts/2023-01/1675102817_sadochok-open.jpg",
-    logo:"http://programki.com.ua/uploads/posts/2023-01/1675102807_sadochok.png",
+    logoHidden:"https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328475523_514032957478274_5135107109279095153_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=ADG1NwuOdzYAX-BCbdz&_nc_ht=scontent.fifo4-1.fna&oh=00_AfAQ9KvsWvhSglx8T18fXA3YqxICu-mzg0yC8B2GRO_8dw&oe=63DCDA13",
+    logo:"https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328559563_516602233689679_6000242912450982717_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=DQHm_1-GGPAAX83sWnq&_nc_ht=scontent.fifo4-1.fna&oh=00_AfAyvvbFCOqET5K_5OPvejMWSEnCLpVL8UAc_SbmAoCfnw&oe=63DDEBFA",
     info:"ТМ «Садочок» — це бренд фруктових та плодоовочевих соків, нектарів, морсів. Виробник  — PepsiCo. Продукція виробляється на заводі з переробки фруктів, овочів та виробництві соків (Миколаївська обл.), заводі з виробництва соків (Миколаївська обл.), двох заводів з переробки фруктів та овочів (Крим та Херсонська обл.).",
 },
 {
     idName:"4",
     answer:"рошен",
-    logoHidden:"http://programki.com.ua/uploads/posts/2023-01/1675102755_roshen-open.jpg",
-    logo:"https://harchi.info/files/companies/roshen_logo.png",
+    logoHidden:"https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328435460_512815957671301_727199244690349913_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=iE-N3gIsfJgAX_7XEKQ&_nc_ht=scontent.fifo4-1.fna&oh=00_AfA5m0Hdtifd_eKxHRG6GfhVPi2ZW8zI8TtHvWnjGY4JSQ&oe=63DDB4D7",
+    logo:"https://scontent.fifo4-1.fna.fbcdn.net/v/t39.30808-6/328329952_919954079374979_1449931315377190297_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=HgMvwBpK_DMAX_dANAw&_nc_ht=scontent.fifo4-1.fna&oh=00_AfA4o727cthCt11wTS5LkOxAOpSkxXpmo_V8HanBOXrA4w&oe=63DD5C24",
     info:"Конди́терська корпора́ція «Роше́н» — один з найбільших виробників кондитерських виробів в Україні та Східній Європі, входить у тридцятку найбільших виробників кондитерських виробів світу. Головний офіс компанії розташовано у Києві.Назва походить з прізвища її засновника — Петра Порошенка шляхом вилучення першого та останнього складів.",
 }
 ]
